d4677f6d5f8f160233c5a7e12b700eced85b0411
Nellie Van Eeckhaute <nve03g@gmail.com>
2025-05-15 15:39:54 +0200

    added GAN model

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+"""
+- load the dataset
+- add noise
+- train CNN model
+- save trained model
+"""
+
 import torch
 from torch.utils.data import DataLoader
 from model import get_model
@@ -6,20 +13,38 @@ import torch.nn.functional as F
 from torch import optim
 import plotly.graph_objects as go
 from tqdm import tqdm
+import os
+import numpy as np
+from torchvision import transforms
+from PIL import Image
+from torch.utils.data import Dataset
+import random
+from datetime import datetime
+import pytz
+tz = pytz.timezone('Europe/Brussels')
 
-
-NUM_EPOCHS = 30
+# hyperparameters for model training
+NUM_EPOCHS = 20
 BATCH_SIZE = 16
 LEARNING_RATE = 1e-3
 NOISE_STD = 0.2
 TRAIN_PATH = 'datasets/train'
-VAL_PATH = 'datasets/val'
+VAL_PATH = 'datasets/validate'
 MODEL_SAVE_PATH = 'denoiser.pth'
-LOSS_PLOT_PATH = 'training_loss_plot.png'
+LOSS_PLOT_PATH_TRAIN = 'training_loss_plot.png'
+
+# change to model_type = "convnet" for convnet
+# change to model_type = "unet" for mini-unet
+# change to model_type = "gan" for GAN-net
+MODEL_TYPE = "gan"
 
 
-def train_model():
+
+
+def train_model(model_type="unet"):
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Device: ", device)
+    print(torch.cuda.is_available())
 
     train_data = NoisyDataset(TRAIN_PATH, noise_std=NOISE_STD)
     val_data = NoisyDataset(VAL_PATH, noise_std=NOISE_STD)
@@ -27,43 +52,83 @@ def train_model():
     train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
     val_loader = DataLoader(val_data, batch_size=BATCH_SIZE)
 
-    # change to model_type = "convnet" for convnet
-    # change to model_type = "unet" for mini-unet
-    model = get_model(model_type='unet').to(device)
-    optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+    model = get_model(model_type=model_type).to(device)
 
-    train_losses = []
-    val_losses = []
+    if model_type == "gan":
+        optimizer_G = optim.Adam(model.generator.parameters(), lr=LEARNING_RATE)  # generator
+        optimizer_D = optim.Adam(model.discriminator.parameters(), lr=LEARNING_RATE)  # discriminator
 
-    for epoch in tqdm(range(NUM_EPOCHS), desc="training process"):
-        model.train()
-        total_train_loss = 0
-        for noisy, clean in train_loader:
-            noisy, clean = noisy.to(device), clean.to(device)
-            out = model(noisy)
-            loss = F.mse_loss(out, clean)
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            total_train_loss += loss.item()
+        train_losses = []
+        val_losses = []
 
-        avg_train_loss = total_train_loss / len(train_loader)
-        train_losses.append(avg_train_loss)
+        for epoch in tqdm(range(NUM_EPOCHS), desc="training GAN"):
+            model.train()
+            g_epoch_loss, d_epoch_loss = 0, 0
 
-        # --- Validation ---
-        model.eval()
-        total_val_loss = 0
-        with torch.no_grad():
-            for noisy, clean in val_loader:
+            for noisy, clean in train_loader:
+                noisy, clean = noisy.to(device), clean.to(device)
+
+                # discriminator
+                optimizer_D.zero_grad()
+                fake_img = model.generator(noisy).detach()
+                real_pred = model.discriminator(clean)
+                fake_pred = model.discriminator(fake_img)
+                d_loss = model.discriminator_loss(real_pred, fake_pred)
+                d_loss.backward()
+                optimizer_D.step()
+
+                # generator
+                optimizer_G.zero_grad()
+                fake_img = model.generator(noisy)
+                fake_pred = model.discriminator(fake_img)
+                g_loss = model.generator_loss(fake_pred, fake_img, clean, epoch=epoch)
+                g_loss.backward()
+                optimizer_G.step()
+
+                g_epoch_loss += g_loss.item()
+                d_epoch_loss += d_loss.item()
+
+            train_losses.append(g_epoch_loss / len(train_loader))
+            val_losses.append(d_epoch_loss / len(train_loader))
+            print(f"Epoch {epoch+1}: G loss: {g_epoch_loss:.4f} | D loss: {d_epoch_loss:.4f}")
+
+        torch.save(model.generator.state_dict(), MODEL_SAVE_PATH)
+
+    else:
+        optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
+
+        train_losses = []
+        val_losses = []
+
+        for epoch in tqdm(range(NUM_EPOCHS), desc="training process"):
+            model.train()
+            total_train_loss = 0
+            for noisy, clean in train_loader:
                 noisy, clean = noisy.to(device), clean.to(device)
                 out = model(noisy)
-                val_loss = F.mse_loss(out, clean)
-                total_val_loss += val_loss.item()
-        avg_val_loss = total_val_loss / len(val_loader)
-        val_losses.append(avg_val_loss)
+                loss = F.mse_loss(out, clean)
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+                total_train_loss += loss.item()
 
+            avg_train_loss = total_train_loss / len(train_loader)
+            train_losses.append(avg_train_loss)
 
-    torch.save(model.state_dict(), MODEL_SAVE_PATH)
+            # --- Validation ---
+            model.eval()
+            total_val_loss = 0
+            with torch.no_grad():
+                for noisy, clean in val_loader:
+                    noisy, clean = noisy.to(device), clean.to(device)
+                    out = model(noisy)
+                    val_loss = F.mse_loss(out, clean)
+                    total_val_loss += val_loss.item()
+            avg_val_loss = total_val_loss / len(val_loader)
+            val_losses.append(avg_val_loss)
+
+        torch.save(model.state_dict(), MODEL_SAVE_PATH)
+
     plot_loss(train_losses, val_losses)
 
 def plot_loss(train_losses, val_losses):
@@ -86,8 +151,12 @@ def plot_loss(train_losses, val_losses):
         yaxis_title='MSE Loss',
         template='plotly_white'
     )
-    fig.write_image(LOSS_PLOT_PATH, width=800, height=600, scale=2)
-    print(f"Plot saved as {LOSS_PLOT_PATH}")
+    fig.write_image(LOSS_PLOT_PATH_TRAIN, width=800, height=600, scale=2)
+    print(f"Plot saved as {LOSS_PLOT_PATH_TRAIN}")
+
+
 
 if __name__ == "__main__":
-    train_model()
+    print("Started at:", datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S"))
+    train_model(model_type=MODEL_TYPE)  # convnet, unet, gan
+    print("Ended at:", datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S"))
